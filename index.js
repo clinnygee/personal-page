3185edc2968ee3a7837381887d0eb8bf0f95cb6e
clinnygee <clintongillespie@outlook.com>
2020-02-03 12:07:55 +1000

    added home, working on about

--- index.js
+++ index.js
@@ -1,48 +1,159 @@
-// let main = document.getElementById('main');
-
-// const navUpdate = (e) => {
-//     console.log(e);
-//     let x = main.scrollTop;
-
-//     console.log(x);
-// }
-
-// main.addEventListener('scroll', navUpdate)
 
 
+// const page = document.querySelector('main');
+// console.log(page)
+// console.log(page);
 
-// console.log(main);
+// const content = document.querySelector('.content');
 
-// need to find the pixel size of the screen
+// const about = document.querySelector('about');
 
-// console.log(document.body.clientHeight)
+// const projects = document.querySelector('projects');
 
-// const height = document.body.clientHeight;
+// const contact = document.querySelector('contact');
 
-// window.onscroll = function(){
-//     navUpdate()
+// // find out how far content is from <main>,
+// // when you scroll to content, change the nav to a fixed position.
+
+// const scrollDistance = content.offsetTop;
+
+// const nav = document.querySelector('nav');
+
+// console.log(nav);
+
+// page.onscroll = function(){
+//     console.log(page.scrollTop)
+//     navUpdate();
 // }
 
 // const navUpdate = () => {
-//     console.log(document.body.scrollTop);
-
-//     if(document.body.scrollTop >= height){
-//         makeNavSticky();
+//     if (page.scrollTop >= scrollDistance){
+//         // set nav to .sticky
+//         nav.classList.add('sticky')
+//     } else {
+//         nav.classList.remove('sticky')
 //     }
-// }
+// };
 
-// const makeNavSticky = () => {
-//     let nav = document.querySelector('.nav');
-//     nav.classList.remove('set')
-//     nav.classList.add('sticky');
+// const navLinks = document.querySelectorAll('.page-link');
 
-//     let stickyNav = nav.cloneNode(true);
+// console.log(navLinks);
 
-//     let body = document.querySelector('body');
+// navLinks[0].addEventListener('click', (e) => {
+//     window.scrollTo({
+//         top: 0,
+//         left: 0,
+//         behavior: 'smooth'
+//     });
+// })
+// console.log(Array.from(document.querySelectorAll('.page-link')));
 
-//     console.log(body);
-//     console.log(stickyNav);
-//     body.appendChild(stickyNav);
+class navigation {
 
-//     console.log(nav)
-// }
+    
+
+    // find the y co-ordinates of each nav item, 
+    // pair it in an object with it's nav link
+    // store the currently active nav item.
+    constructor (){
+        console.log('in constructor')
+        const navLinks = document.querySelectorAll('.page-link');
+
+        this.contentScrollDistance = document.querySelector('.content').offsetTop;
+
+        this.nav = document.querySelector('.nav');
+
+        this.header = {element: navLinks[0], position: 0};
+        this.about = {element: navLinks[1], position: this.contentScrollDistance};
+        this.projects = {element: navLinks[2], position:  document.querySelector('.projects').offsetTop};
+        this.contact = {element: navLinks[3], position:  document.querySelector('.contact').offsetTop};
+
+        this.active = this.header;
+
+        // add event listeners to all nav elements, that will scroll to the correct Y co-ordinates of the screen.
+
+        this.contact.element.addEventListener('click', (e) => {
+            window.scrollTo({
+                top: this.contact.position,
+                left: 0,
+                behavior: "smooth",
+            });
+            this.updateActive(this.contact)
+        });
+
+        this.about.element.addEventListener('click', (e) => {
+            window.scrollTo({
+                top: this.about.position,
+                left: 0,
+                behavior: "smooth",
+            });
+            this.updateActive(this.about);
+        })
+
+        this.projects.element.addEventListener('click', (e) => {
+            window.scrollTo({
+                top: this.projects.position,
+                left: 0,
+                behavior: "smooth",
+            });
+            this.updateActive(this.projects);
+        })
+
+        this.header.element.addEventListener('click', (e) => {
+            window.scrollTo({
+                top: this.header.position,
+                left: 0,
+                behavior: "smooth",
+            });
+            this.updateActive(this.header);
+        })
+        console.log(this.header)
+        console.log(this.about);
+        console.log(this.projects);
+        console.log(this.contact)
+        this.page = document.querySelector('body');
+
+        
+
+
+        window.addEventListener('scroll', () => {
+            
+            this.navUpdate();
+        })
+        console.log(this.page)
+    };
+
+    navUpdate = () => {
+        
+        if (this.page.scrollTop >= this.contentScrollDistance){
+            // set nav to .sticky
+            this.nav.classList.add('sticky')
+            // need to set section to about
+
+            // first find element with .active, and remove.active
+
+
+        } else {
+            this.nav.classList.remove('sticky')
+        }
+    };
+
+    updateActive = (element) => {
+        // clears the .active class from this.active
+        console.log(this.active.element);
+        this.active.element.classList.remove('active');
+        console.log(this.active);
+
+        this.active = element;
+        console.log(this.active)
+
+        this.active.element.classList.add('active');
+        console.log(this.active);
+    }
+};
+
+let navigator = new navigation();
+
+
+
+// console.log(content.offsetTop);
